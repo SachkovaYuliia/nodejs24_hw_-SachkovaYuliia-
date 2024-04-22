04cf8fe80f61bf3f6fed418295c15edd7c052c77
evhenious <evhenious@gmail.com>
2024-04-22 22:34:10 +0300

    no need to keep old code in comments, it is always available in git

--- main.js
+++ main.js
@@ -4,24 +4,3 @@ const logger = require('./utils/logger')('main');
 logger.info('the script is running!');
 logger.warn('another script is running!');
 logger.error('the script is not running!');
-
-// та передаємо повідомлення в створені в модулі logger методи: 
-// logger.info('My first homework is here');
-// logger.warn('Is everything alright?');
-// logger.error('No way!');
-
-// або ж імпортуємо та передаємо інфорацію по одному:
-
-// const {info} = require('./utils/logger');
-// info('My first homework is here');
-// const {warn} = require('./utils/logger');
-// warn('Is everything alright?');
-// const {error} = require('./utils/logger');
-// error('No way!');
-
-// або вивiд напряму:
-// const {info, warn, error} = require('./utils/logger')();
-// info('My first homework is here');
-// warn('Is everything alright?');
-// error('No way!');
-
--- utils/logger.js
+++ utils/logger.js
@@ -1,31 +1,3 @@
-// реалізовуємо три методи, та експортуємо три ключа об'єктом, для того, щоб вони були доступні для використання ззовні цього модуля.
-// function info(msg){
-//     console.log("[INFO]", msg);
-// };
-// function warn(msg){
-//     console.error("[WARN]", msg);
-// };
-// function error(msg){
-//     console.error("[ERROR]", msg);
-// };
-// module.exports = {
-//     info,
-//     warn,
-//     error
-// }
-
-// або можемо відразу повертати ключі:
-// function getlLogger(){
-//     console.log('one more way to get logger');
-    
-//     return {
-//         info,
-//         warn,
-//         error
-//     };
-// }
-
-
 function getLogger(prefix) {
     return {
         info: function(msg) {
