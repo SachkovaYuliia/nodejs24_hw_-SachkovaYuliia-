33f74305ec0d9c0600653d563d874f079cb1b7ce
evhenious <evhenious@gmail.com>
2024-04-29 20:52:21 +0300

    app config applied to logger

--- utils/logger.js
+++ utils/logger.js
@@ -1,27 +1,25 @@
-const dotenv = require('dotenv');
-dotenv.config();
 const colors = require('colors');
+const { colorsEnabled, logLevel } = require('config');
 
-const COLORS_ENABLED = process.env.COLORS_ENABLED;
-const LOG_LEVEL = process.env.LOG_LEVEL;
-
-console.log(COLORS_ENABLED);
-console.log(LOG_LEVEL);
+//! цю частину про виключення кольорів ти десь взагалі загубила
+if (!colorsEnabled) {
+  colors.disable();
+}
 
 const getLogger = {
   info: (moduleName, msg) => {
-      if (LOG_LEVEL === 'info') {
-          console.log(`${colors.bgGreen(moduleName)}: ${msg}`);
-      }
+    if (logLevel === 'info') {
+      console.log(`${colors.bgGreen(moduleName)}: ${msg}`);
+    }
   },
   warn: (moduleName, msg) => {
-      if (LOG_LEVEL === 'warn' || LOG_LEVEL === 'info') {
-          console.log(`${colors.bgBlue(moduleName)}: ${msg}`);
-      }
+    if (logLevel === 'warn' || logLevel === 'info') {
+      console.error(`${colors.bgBlue(moduleName)}: ${msg}`);
+    }
   },
-  error: (moduleName, msg) =>  {
-          console.log(`${colors.bgReded(moduleName)}: ${msg}`);
-  }
+  error: (moduleName, msg) => {
+    console.error(`${colors.bgRed(moduleName)}: ${msg}`);
+  },
 };
 
 module.exports = getLogger;
