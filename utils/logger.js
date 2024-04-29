6474958a1fab565fd3f80a38a78c64a939a4924b
Yuliia Sachkova <yuliasachkova@gmail.com>
2024-04-29 18:04:19 +0200

    hw2 edition3 :)

--- utils/logger.js
+++ utils/logger.js
@@ -1,10 +1,6 @@
 const dotenv = require('dotenv');
 dotenv.config();
-const {bgGreen, bgBlue, bgRed} = require('colors/safe');
-
-// const config = require('config');
-// const COLORS_ENABLED = config.get('COLORS_ENABLED');
-// const LOG_LEVEL = config.get('LOG_LEVEL');
+const colors = require('colors');
 
 const COLORS_ENABLED = process.env.COLORS_ENABLED;
 const LOG_LEVEL = process.env.LOG_LEVEL;
@@ -12,39 +8,22 @@ const LOG_LEVEL = process.env.LOG_LEVEL;
 console.log(COLORS_ENABLED);
 console.log(LOG_LEVEL);
 
-function getModuleColor(col) {
-    if (COLORS_ENABLED === 1) {
-      switch (col) {
-        case 'info':
-          return bgGreen;
-        case 'warn':
-          return bgBlue;
-        case 'error':
-          return bgRed;
-        default:
-          return moduleName => moduleName; 
-      }
-    } else {
-      return moduleName => moduleName; 
-    }
-  }
-  
-  const getLogger = {
-    info: (moduleName, msg) => {
+const getLogger = {
+  info: (moduleName, msg) => {
       if (LOG_LEVEL === 'info') {
-        console.log(`${getModuleColor('info')(moduleName)}: ${msg}`);
+          console.log(`${colors.bgGreen(moduleName)}: ${msg}`);
       }
-    },
-    warn: (moduleName, msg) => {
+  },
+  warn: (moduleName, msg) => {
       if (LOG_LEVEL === 'warn' || LOG_LEVEL === 'info') {
-        console.log(`${getModuleColor('warn')(moduleName)}: ${msg}`);
+          console.log(`${colors.bgBlue(moduleName)}: ${msg}`);
       }
-    },
-    error: (moduleName, msg) => {
-      if (LOG_LEVEL === 'error'|| LOG_LEVEL === 'info'|| LOG_LEVEL === 'warn') {
-        console.log(`${getModuleColor('error')(moduleName)}: ${msg}`);
+  },
+  error: (moduleName, msg) => {
+      if (LOG_LEVEL === 'error' || LOG_LEVEL === 'info' || LOG_LEVEL === 'warn') {
+          console.log(`${colors.bgReded(moduleName)}: ${msg}`);
       }
-    }
-  };
+  }
+};
 
 module.exports = getLogger;
