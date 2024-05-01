c33dd3d20914e9b9006c9601bfcbd1b4b9431765
Yuliia Sachkova <yuliasachkova@gmail.com>
2024-05-01 21:03:48 +0200

    Added t wo catalogues: source and target; added new module file_sync.

--- main.js
+++ main.js
@@ -8,3 +8,6 @@ const logger = require('./utils/logger');
 logger.info('Module', 'the script is running!');
 logger.warn('Module', 'another script is running!');
 logger.error('Module', 'the script is not running!');
+
+const fileSync = require('./file_sync');
+fileSync.start();
