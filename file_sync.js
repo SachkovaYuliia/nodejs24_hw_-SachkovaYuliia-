f3795b207cb9455bbaaa76d469f8f4dd7c7d05d8
evhenious <evhenious@gmail.com>
2024-05-05 12:33:33 +0300

    simplified file sync module

--- file_sync.js
+++ file_sync.js
@@ -1,58 +1,43 @@
-const fs = require('fs');
 const fsAsync = require('fs/promises');
-const fsExtra = require('fs-extra');
 const path = require('path');
+const logger = require('./utils/logger')('file sync'); //! не забуваємо про наш логгер
 
 async function copyDirectoryAsync(sourceDir, targetDir) {
-    try {
-        await fsExtra.copy(sourceDir, targetDir, { overwrite: false });
-        console.info('Directory copy successful');
+    // потрібен всього один цикл по контенту. Всередині, ми дивимось що це за айтем
+    const sourceItems = await fsAsync.readdir(sourceDir); // саме "items" - тому що там і файли, і каталоги разом
+    const targetItems = await fsAsync.readdir(targetDir);
 
-        const sourceFiles = await fsAsync.readdir(sourceDir);
-        const targetFiles = await fsAsync.readdir(targetDir);
+    for (const item of sourceItems) {
+        const sourceItemPath = path.join(sourceDir, item);
+        const targetItemPath = path.join(targetDir, item);
 
-        for (const file of sourceFiles) {
-            const sourcePath = path.join(sourceDir, file);
-            const targetPath = path.join(targetDir, file);
-
-            if (!targetFiles.includes(file)) {
-                await fsAsync.copyFile(sourcePath, targetPath);
-                console.info(`The file ${file} was successfully copied to the target directory.`);
-            }
+        if (targetItems.includes(item)) {
+            logger.warn(`${item} already exists in ${targetDir}`);
+            continue; // айтем вже є в target, переходимо до наступного
         }
 
-        for (const file of sourceFiles) {
-            const sourcePath = path.join(sourceDir, file);
-            const targetPath = path.join(targetDir, file);
-
-            const stats = await fsAsync.stat(sourcePath);
-            const targetStats = await fsAsync.stat(targetPath);
-
-            if (stats.isDirectory()) {
-                if (!targetStats || !targetStats.isDirectory()) {
-                    await fsAsync.mkdir(targetPath);
-                }
-                await copyDirectoryAsync(sourcePath, targetPath);
-            } else {
-                if (!targetFiles.includes(file)) {
-                    await fsAsync.copyFile(sourcePath, targetPath);
-                    console.info(`The file ${file} was successfully copied to the target directory.`);
-                } else {
-                    console.warn(`The file ${file} already exists in the target directory.`);
-                }
-            }
+        // якщо файл - копіюємо, якщо каталог - створюємо такий самий каталог в target і перекидаємось на нього як на новий source
+        const sourceItemStats = await fsAsync.stat(sourceItemPath);
+        if (sourceItemStats.isFile()) {
+            fsAsync.copyFile(sourceItemPath, targetItemPath);
+            logger.info(`File ${targetItemPath} copied successfully`);
+            continue; // це був файл, його не було в target, скопіювали і переходимо до наступної ітерації циклу
         }
-    } catch (err) {
-        console.error('Error copying directory:', err);
+
+        if (sourceItemStats.isDirectory()) {
+            await fsAsync.mkdir(targetItemPath);
+            logger.info(`Directory ${targetItemPath} created`);
+            await copyDirectoryAsync(sourceItemPath, targetItemPath);
+        }
     }
 }
 
 async function start() {
     try {
         await copyDirectoryAsync('./source', './target');
-        console.info('Directory copy successful');
+        logger.info('File synchronization finished');
     } catch (err) {
-        console.error('Error copying directory:', err);
+        logger.error('File sync process failed:', err);
     }
 }
 
