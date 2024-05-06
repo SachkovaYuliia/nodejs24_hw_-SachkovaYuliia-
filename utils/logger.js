eebd327ece43ab0c21be24cb7dadeaf80a845d94
Yuliia Sachkova <yuliasachkova@gmail.com>
2024-05-07 00:05:52 +0200

    added createdLogsFolder function

--- utils/logger.js
+++ utils/logger.js
@@ -1,7 +1,10 @@
 const colors = require('colors');
 const { colorsEnabled, logLevel } = require('config');
+const fs = require('fs');
+const path = require('path');
+const { Readable, Writable } = require('stream');
 
-//! цю частину про виключення кольорів ти десь взагалі загубила
+// ! цю частину про виключення кольорів ти десь взагалі загубила
 if (!colorsEnabled) {
   colors.disable();
 }
@@ -21,5 +24,25 @@ const getLogger = (moduleName) => ({
     console.error(`${colors.bgRed(moduleName)}:`, ...msg);
   },
 });
+
+// _____hw4
+
+function createLogsFolder() {
+    const logsDir = path.join(__dirname, 'logs');
+    if (!fs.existsSync(logsDir)) {
+        fs.mkdirSync(logsDir);
+    }
+    const infoLogFile = path.join(logsDir, 'info.log');
+    const errorLogFile = path.join(logsDir, 'errors.log');
+    if (!fs.existsSync(infoLogFile)) {
+        fs.writeFileSync(infoLogFile, ''); 
+    }
+    if (!fs.existsSync(errorLogFile)) {
+        fs.writeFileSync(errorLogFile, ''); 
+    }
+}
+
+createLogsFolder();
+
 
 module.exports = getLogger;
